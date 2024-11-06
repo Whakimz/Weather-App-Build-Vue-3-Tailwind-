2814f5063480a006b36532cffdaccb7c51e4aca8
THISKIM\khimk <khimk635@gmail.com>
2024-11-07 06:38:58 +0700

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
+  <SiteNavigation />
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
-    <SiteNavigation />
-    <RouterView v-slot="{ Component }">
-      <Transition name="page" mode="out-in">
-        <component :is="Component" />
-      </Transition>
-    </RouterView>
+    <div class="flex-1">
+      <RouterView v-slot="{ Component }">
+        <Transition name="page" mode="out-in">
+          <component :is="Component" />
+        </Transition>
+      </RouterView>
+    </div>
   </div>
+  <FootersSite />
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router'
 import SiteNavigation from '@/components/SiteNavigation.vue'
+import FootersSite from './components/FootersSite.vue'
 </script>
 
 <style>
---
+++ src/components/FootersSite.vue
@@ -0,0 +1,71 @@
+<template>
+  <footer class="bg-weather-secondary text-white py-8">
+    <div class="container mx-auto px-4">
+      <!-- Contact Information -->
+      <div class="flex flex-col md:flex-row items-center justify-between">
+        <div class="mb-4 md:mb-0">
+          <h2 class="text-lg font-semibold">Contact Us</h2>
+          <p class="text-gray-400">Email: devkim1910@gmail.com</p>
+          <p class="text-gray-400">Phone: +66 94 371 8956</p>
+        </div>
+
+        <!-- Social Media Links -->
+        <div class="flex space-x-6">
+          <!-- Facebook -->
+          <a
+            href="https://www.facebook.com/surapat.wongsri.1/"
+            class="text-gray-400 hover:text-white"
+            aria-label="Facebook"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              class="w-6 h-6"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M22 12a10 10 0 1 0-11.68 9.85V14.9h-3.1v-3h3.1V9.7c0-3.1 1.87-4.8 4.67-4.8 1.34 0 2.74.24 2.74.24v3h-1.55c-1.53 0-2 .95-2 1.93v2.23h3.4l-.54 3H15.2v6.95A10 10 0 0 0 22 12Z"
+              />
+            </svg>
+          </a>
+
+          <!-- GitHub -->
+          <a
+            href="https://github.com/Whakimz"
+            class="text-gray-400 hover:text-white"
+            aria-label="GitHub"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              class="w-6 h-6"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M12 .5C5.4.5 0 5.9 0 12.5c0 5.3 3.4 9.8 8 11.4.6.1.8-.2.8-.5v-1.7c-3.3.7-4-.7-4-.7-.5-1.2-1.2-1.6-1.2-1.6-.9-.6.1-.6.1-.6 1 .1 1.6 1 1.6 1 .9 1.5 2.4 1.1 3 .8.1-.6.4-1 .7-1.3-2.7-.3-5.5-1.4-5.5-6.1 0-1.4.5-2.5 1.2-3.4-.1-.3-.5-1.5.1-3.1 0 0 1-.3 3.3 1.3.9-.2 1.9-.4 2.9-.4 1 0 2 .1 2.9.4 2.3-1.6 3.3-1.3 3.3-1.3.6 1.6.2 2.8.1 3.1.8.9 1.2 2 1.2 3.4 0 4.7-2.8 5.8-5.5 6.1.4.3.8 1 .8 2v2.9c0 .3.2.6.8.5 4.6-1.6 8-6.1 8-11.4C24 5.9 18.6.5 12 .5Z"
+              />
+            </svg>
+          </a>
+
+          <!-- Instagram -->
+          <a
+            href="https://www.instagram.com/w4kims/?hl=th"
+            class="text-gray-400 hover:text-white"
+            aria-label="Instagram"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              class="w-6 h-6"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M7.75 2h8.5A5.76 5.76 0 0 1 22 7.75v8.5A5.76 5.76 0 0 1 16.25 22h-8.5A5.76 5.76 0 0 1 2 16.25v-8.5A5.76 5.76 0 0 1 7.75 2Zm0 1.5a4.25 4.25 0 0 0-4.25 4.25v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5A4.25 4.25 0 0 0 20.5 16.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5ZM12 7.25A4.75 4.75 0 1 1 7.25 12 4.75 4.75 0 0 1 12 7.25Zm0 1.5a3.25 3.25 0 1 0 3.25 3.25A3.25 3.25 0 0 0 12 8.75ZM17 6.25a.75.75 0 1 1-.75.75.75.75 0 0 1 .75-.75Z"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
